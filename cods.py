6c8bb3f936d7d05d43e284855dc08d0b94373357
Mike <rans@un.org>
2021-01-27 14:04:00 +1300

    Fix test

--- cods.py
+++ cods.py
@@ -16,90 +16,104 @@ from hdx.data.organization import Organization
 from hdx.data.resource import Resource
 from hdx.data.showcase import Showcase
 from hdx.location.country import Country
+from hdx.utilities import get_uuid
 from hdx.utilities.dateparse import parse_date
 from slugify import slugify
 
 logger = logging.getLogger(__name__)
 
 
-def get_datasets_metadata(url, downloader):
-    response = downloader.download(url)
-    return response.json()
+class COD:
+    def __init__(self, downloader):
+        self.downloader = downloader
+        self.batches_by_org = dict()
 
+    def get_dataset_titles(self, url):
+        headers, iterator = self.downloader.get_tabular_rows(url, dict_form=True)
+        return [x['Dataset title'] for x in iterator]
 
-def generate_dataset(metadata):
-    title = metadata['DatasetTitle']
-    if metadata['Total'] == 0:
-        logger.warning(f'Ignoring dataset: {title} which has no resources!')
-        return None
-    logger.info(f'Creating dataset: {title}')
-    if not metadata['Source']:
-        return None
-    dataset = Dataset({
-        'name': slugify(title[:99]),
-        'title': title,
-        'notes': metadata['DatasetDescription'],
-        'dataset_source': metadata['Source'],
-        'methodology': metadata['Methodology'],
-        'methodology_other': metadata['Methodology_Other'],
-        'license_id': metadata['License'],
-        'license_other': metadata['License_Other'],
-        'caveats': metadata['Caveats'],
-        'data_update_frequency': metadata['FrequencyUpdates'],
-        'cod_level': 'cod-enhanced'
-    })
-    licence = metadata['License']
-    if licence == 'Other':
-        dataset['license_id'] = 'hdx-other'
-        dataset['license_other'] = metadata['License_Other']
-    else:
-        dataset['license_id'] = licence
-    methodology = metadata['Methodology']
-    if methodology == 'Other':
-        dataset['methodology'] = 'Other'
-        methodology_other = metadata['Methodology_Other']
-        if not methodology_other:
+    def get_datasets_metadata(self, url, dataset_titles=None):
+        response = self.downloader.download(url)
+        results = response.json()
+        if dataset_titles is None:
+            return results
+        return [x for x in results if x['DatasetTitle'] in dataset_titles]
+
+    def generate_dataset(self, metadata):
+        title = metadata['DatasetTitle']
+        if metadata['Total'] == 0:
+            logger.warning(f'Ignoring dataset: {title} which has no resources!')
             return None
-        dataset['methodology_other'] = methodology_other
-    else:
-        dataset['methodology'] = methodology
-    dataset.set_maintainer('196196be-6037-4488-8b71-d786adf4c081')
-    organization = Organization.autocomplete(metadata['Contributor'])
-    dataset.set_organization(organization[0]['id'])
-    dataset.set_subnational(True)
-    dataset.add_country_locations(metadata['Location'])
-    dataset.add_tags(metadata['Tags'])
+        logger.info(f'Creating dataset: {title}')
+        if not metadata['Source']:
+            logger.warning(f'Ignoring dataset: {title} which has no source!')
+            return None
+        dataset = Dataset({
+            'name': slugify(title[:99]),
+            'title': title,
+            'notes': metadata['DatasetDescription'],
+            'dataset_source': metadata['Source'],
+            'methodology': metadata['Methodology'],
+            'methodology_other': metadata['Methodology_Other'],
+            'license_id': metadata['License'],
+            'license_other': metadata['License_Other'],
+            'caveats': metadata['Caveats'],
+            'data_update_frequency': metadata['FrequencyUpdates'],
+            'cod_level': 'cod-enhanced'
+        })
+        licence = metadata['License']
+        if licence == 'Other':
+            dataset['license_id'] = 'hdx-other'
+            dataset['license_other'] = metadata['License_Other']
+        else:
+            dataset['license_id'] = licence
+        methodology = metadata['Methodology']
+        if methodology == 'Other':
+            dataset['methodology'] = 'Other'
+            methodology_other = metadata['Methodology_Other']
+            if not methodology_other:
+                logger.warning(f'Ignoring dataset: {title} which has no methodology!')
+                return None
+            dataset['methodology_other'] = methodology_other
+        else:
+            dataset['methodology'] = methodology
+        dataset.set_maintainer('196196be-6037-4488-8b71-d786adf4c081')
+        organization = Organization.autocomplete(metadata['Contributor'])
+        organization_id = organization[0]['id']
+        dataset.set_organization(organization_id)
+        batch = self.batches_by_org.get(organization_id, get_uuid())
+        self.batches_by_org[organization_id] = batch
+        dataset.set_subnational(True)
+        dataset.add_country_locations(metadata['Location'])
+        dataset.add_tags(metadata['Tags'])
 
-    startdate = None
-    enddate = None
-    ongoing = False
-    resources = list()
-    for resource_metadata in metadata['Resources']:
-        resource_daterange = resource_metadata['daterange_for_data']
-        resourcedata = {
-            'name': resource_metadata['ResourceItemTitle'],
-            'description': resource_metadata['ResourceItemDescription'],
-            'url': resource_metadata['DownloadURL'],
-            'format': resource_metadata['Format'],
-            'daterange_for_data': resource_daterange,
-            'grouping': resource_metadata['Version']
-        }
-        date_info = DateHelper.get_date_info(resource_daterange)
-        resource_startdate = date_info['startdate']
-        resource_enddate = date_info['enddate']
-        resource_ongoing = date_info['ongoing']
-        if startdate is None or resource_startdate < startdate:
-            startdate = resource_startdate
-        if enddate is None or resource_enddate > enddate:
-            enddate = resource_enddate
-            ongoing = resource_ongoing
-        if resource_ongoing:
-            resource_enddate = '*'
-        resource = Resource(resourcedata)
-        resource.set_date_of_resource(resource_startdate, resource_enddate)
-        resources.append(resource)
-    if ongoing:
-        enddate = '*'
-    dataset.set_date_of_dataset(startdate, enddate)
-    dataset.add_update_resources(resources)
-    return dataset
+        startdate = None
+        enddate = None
+        ongoing = False
+        resources = list()
+        for resource_metadata in metadata['Resources']:
+            resource_daterange = resource_metadata['daterange_for_data']
+            resourcedata = {
+                'name': resource_metadata['ResourceItemTitle'],
+                'description': resource_metadata['ResourceItemDescription'],
+                'url': resource_metadata['DownloadURL'],
+                'format': resource_metadata['Format'],
+                'daterange_for_data': f'[{resource_daterange}]',
+                'grouping': resource_metadata['Version']
+            }
+            date_info = DateHelper.get_date_info(resource_daterange)
+            resource_startdate = date_info['startdate']
+            resource_enddate = date_info['enddate']
+            resource_ongoing = date_info['ongoing']
+            if startdate is None or resource_startdate < startdate:
+                startdate = resource_startdate
+            if enddate is None or resource_enddate > enddate:
+                enddate = resource_enddate
+                ongoing = resource_ongoing
+            resource = Resource(resourcedata)
+            resources.append(resource)
+        if ongoing:
+            enddate = '*'
+        dataset.set_date_of_dataset(startdate, enddate)
+        dataset.add_update_resources(resources)
+        return dataset, batch
--- run.py
+++ run.py
@@ -9,11 +9,10 @@ from os.path import join, expanduser
 
 from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
-from hdx.utilities.path import progress_storing_tempdir
 
 from hdx.facades.simple import facade
 
-from cods import get_datasets_metadata, generate_dataset
+from cods import COD
 
 logger = logging.getLogger(__name__)
 
@@ -24,15 +23,16 @@ def main():
     """Generate dataset and create it in HDX"""
 
     configuration = Configuration.read()
-    url = configuration['url']
     with Download() as downloader:
-        datasets_metadata = get_datasets_metadata(url, downloader)
+        cod = COD(downloader)
+        dataset_titles = cod.get_dataset_titles(configuration['config_url'])
+        datasets_metadata = cod.get_datasets_metadata(configuration['url'], dataset_titles=dataset_titles)
         logger.info('Number of datasets to upload: %d' % len(datasets_metadata))
-        for info, metadata in progress_storing_tempdir('CODS', datasets_metadata, 'DatasetTitle'):
-            dataset = generate_dataset(metadata)
+        for metadata in datasets_metadata:
+            dataset, batch = cod.generate_dataset(metadata)
             if dataset:
                 dataset.update_from_yaml()
-                dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False, updated_by_script='HDX Scraper: CODS', batch=info['batch'])
+                dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False, updated_by_script='HDX Scraper: CODS', batch=batch)
 
 
 if __name__ == '__main__':
